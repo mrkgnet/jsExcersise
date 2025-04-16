9f30e1657dc3b59f2aeca43fa3df16ed756c32a0
mrkgnet <roztainfo@gmail.com>
2025-04-16 13:03:55 +0330

    JavaScript String Methods

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-<html>
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
 <body>
-
-<h1>My Web Page</h1>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
-</script>
-
+    <scropt>
+        const 
+    </scropt>
 </body>
-</html> 
+</html>
